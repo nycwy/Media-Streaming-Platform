9feefb92723b91a711f0189baa2b0cbe72bbc1f2
nycwy <stuxy.1129@gmail.com>
2026-01-01 01:30:17 +0545

    throw error if user is not created as expected

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -52,6 +52,14 @@ const registerUser = asyncHandler(async (req, res) => {
         username: username.toLowerCase()
     });
 
+    const createdUser = await User.findById(user._id).select(
+        "-password -refreshToken"
+    )
+
+    if (!createdUser) {
+        throw new ApiError(500, "Something went wrong while registering the user");
+    }
+
 });
 
 export { registerUser }
